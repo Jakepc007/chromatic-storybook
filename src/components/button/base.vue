12ec754bddcb62e926bf108de21c7660afa5a91b
Jake Morris <Jakemorrishello@gmail.com>
2023-10-18 14:11:19 +0200

    base

---
+++ src/components/button/base.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+const props = defineProps<{
+  type: 'emoji-only' | 'text-only' | 'emoji-and-text'
+}>()
+
+const emits = defineEmits<{
+  
+}>()
+</script>
+
+<template>
+  <div>
+    <button type="button" :class="classes" @click="onClick"><slot></slot></button>
+  </div>    
+</template>
